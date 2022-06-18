b09eed8b5aa02a5dedc709347a7a46370446e019
Nirmaljeet singh <niramaljeetsingh@Niramaljeets-MacBook-Air.local>
2022-06-18 11:49:21 +0530

    new changes

--- middleware/adminauth.js
+++ middleware/adminauth.js
@@ -7,15 +7,15 @@ const isAdmin = async(req,res,next) => {
     let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
     let jwtSecretKey = process.env.JWT_SECRET_KEY;
   
-    try {
-        const verified = jwt.verify(token, jwtSecretKey);
-        if(!verified){
-            return res.send("Successfully Verified");
-        }
-    } catch (error) {
-        // Access Denied
-        return res.status(401).send(error);
-    }
+    // try {
+    //     const verified = jwt.verify(token, jwtSecretKey);
+    //     if(!verified){
+    //         return res.send("Successfully Verified");
+    //     }
+    // } catch (error) {
+    //     // Access Denied
+    //     return res.status(401).send(error);
+    // }
     next();
 }
 module.exports = isAdmin;
