05a2903f717cfaf59df890726ec5aa1e97b32954
Nirmaljeet singh <niramaljeetsingh@Niramaljeets-MacBook-Air.local>
2022-06-18 12:13:05 +0530

    node module remove

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,16 @@
 const express = require('express');
 
 const verify = async(req,res,next) => {
-    const token = req.headers['secret-key'] || '';
+    const token = req.headers['secret-key'] ?? '';
 
-    // if(!token) return res.status(401).send({message:'Un-authorized !!'});
-    // try {
-    //     // const isVerified = await jwt.verify(token,jwtSecretKey);
-    //     const token_app = '#1234nirmaljeetdeveloper987654321#satta#free#react#**project123456789#./';
-    //     if(token != token_app) return res.status(401).send({message:'Invlaid token !!'});
-    // } catch (error) {
-    //     return res.status(401).send({message:'Invlaid token !!'});
-    // }
+    if(!token) return res.status(401).send({message:'Un-authorized !!'});
+    try {
+        // const isVerified = await jwt.verify(token,jwtSecretKey);
+        const token_app = '#1234nirmaljeetdeveloper987654321#satta#free#react#**project123456789#./';
+        if(token != token_app) return res.status(401).send({message:'Invlaid token !!'});
+    } catch (error) {
+        return res.status(401).send({message:'Invlaid token !!'});
+    }
 
     next();
 }
