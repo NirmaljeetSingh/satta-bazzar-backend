5ea890c975a922463eba394ec4b33c2b1c39760a
Nirmaljeet singh <niramaljeetsingh@Niramaljeets-MacBook-Air.local>
2022-07-02 12:29:45 +0530

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var cors = require('cors')
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 
-app.use(cors());
+app.use(cors('*'));
 // app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
