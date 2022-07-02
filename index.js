3b3f6cb38c169e885d82d9b9ee4d6ff6fce9003a
Nirmaljeet singh <niramaljeetsingh@Niramaljeets-MacBook-Air.local>
2022-07-02 13:11:59 +0530

    cors

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ var cors = require('cors')
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 
-app.use(cors('*'));
+
+app.use(cors({
+    origin: "*"
+})) // Use this after the variable declaration
+
+// app.use(cors('*'));
 // app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
